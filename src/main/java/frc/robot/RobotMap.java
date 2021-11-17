8fbd3319264d573599b1751ab0010d83c3eb8f91
Megan-Tian <chtkeke@gmail.com>
2021-11-16 18:01:10 -0800

    first commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class RobotMap {
+    //joysticks
+    public static final int RIGHT_JOYSTICK = 1;
+	public static final int LEFT_JOYSTICK = 0;
+
+	public static final int FRONT_RIGHT_DRIVE_CHANNEL = 1;
+	public static final int BACK_RIGHT_DRIVE_CHANNEL = 2;
+	public static final int FRONT_LEFT_DRIVE_CHANNEL = 3;
+	public static final int BACK_LEFT_DRIVE_CHANNEL = 4;
+}
